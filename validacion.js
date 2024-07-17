74a9a239459db36c8e041fffbecf0744d7cf7130
Yaidi <yaidi2001@gmail.com>
2024-07-16 23:24:57 -0600

    casi todo

--- validacion.js
+++ validacion.js
@@ -1,1 +1,26 @@
 //Haz tú validación en javascript acá
+const form = document.querySelector("#form")
+
+function verificarForm (evento) {
+    evento.preventDefault();
+    const nombre = document.querySelector(".nombre").value;
+    const email = document.querySelector("email").value;
+    const asunto = document.querySelector(".asunto").value;
+    const mensaje = document.querySelector(".mensaje").value;
+
+    if (isVacio(nombre)) {
+        
+    } else {
+        
+    }
+}
+
+form.addEventListener("submit", evento => verificarForm(evento));
+
+function verificarSize(string, numeroCar){
+    string.length <= numeroCar
+}
+
+function isVacio(string) {
+    string.length == 0
+}
